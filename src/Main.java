1ee8c9d54ac162968e8dd8972cf5fef1ef61de9a
K1ndbeetle <alx-s@bk.ru>
2022-12-14 22:24:45 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,13 @@ public class Main {
         System.out.println("Задача №1");
         int year = LocalDate.now().getYear();
         yearCheck (year);
+        System.out.println();
+
+        //task №2
+        System.out.println("Задача №2");
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+        checkOS (clientOS, clientDeviceYear);
     }
 
     //task №1
@@ -15,5 +22,22 @@ public class Main {
         } else {
             System.out.println(year + " год не является високосным");
         }
+    }
+
+    //task №2
+    public static void checkOS (int clientOS, int clientDeviceYear) {
+         if (clientOS == 0 && clientDeviceYear >= 2015) {
+             System.out.println("Установите версию приложения для iOS по ссылке");
+         } else if (clientOS == 0 && clientDeviceYear < 2015) {
+             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+         }
+         if (clientOS == 1 && clientDeviceYear >= 2015) {
+             System.out.println("Установите версию приложения для Android по ссылке");
+         } else if (clientOS == 1 && clientDeviceYear < 2015) {
+             System.out.println("Установите облегченную версию приложения для Android по ссылке");
+         }
+         if (clientOS < 0 || clientOS > 2) {
+             System.out.println("У вас неизвестная версия ОС");
+         }
     }
 }

8e019dcf4ec20c6aa334f29d788345c9052aa160
K1ndbeetle <alx-s@bk.ru>
2022-12-14 22:23:19 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task №1
+        System.out.println("Задача №1");
+        int year = LocalDate.now().getYear();
+        yearCheck (year);
+    }
+
+    //task №1
+    public static void yearCheck (int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }

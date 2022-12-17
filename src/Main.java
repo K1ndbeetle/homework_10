c5c3589b7776bedbaf3e7b6182736c895e67d245
K1ndbeetle <alx-s@bk.ru>
2022-12-17 11:37:40 +0300

    Task2 corrected

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,16 @@ public class Main {
     public static void main(String[] args) {
         //task №1
         System.out.println("Задача №1");
-        int year = LocalDate.now().getYear();
+        int year = 2018;
         yearCheck (year);
         System.out.println();
 
         //task №2
         System.out.println("Задача №2");
         int clientOS = 0;
-        int clientDeviceYear = 2015;
-        checkOS (clientOS, clientDeviceYear);
+        int clientDeviceYear = 2021;
+        int currentYear = LocalDate.now().getYear();
+        checkOS (clientOS, clientDeviceYear, currentYear);
         System.out.println();
 
         //task №3
@@ -32,15 +33,15 @@ public class Main {
     }
 
     //task №2
-    public static void checkOS (int clientOS, int clientDeviceYear) {
-         if (clientOS == 0 && clientDeviceYear >= 2015) {
+    public static void checkOS (int clientOS, int clientDeviceYear, int currentYear) {
+         if (clientOS == 0 && clientDeviceYear >= currentYear) {
              System.out.println("Установите версию приложения для iOS по ссылке");
-         } else if (clientOS == 0 && clientDeviceYear < 2015) {
+         } else if (clientOS == 0 && clientDeviceYear < currentYear) {
              System.out.println("Установите облегченную версию приложения для iOS по ссылке");
          }
-         if (clientOS == 1 && clientDeviceYear >= 2015) {
+         if (clientOS == 1 && clientDeviceYear >= currentYear) {
              System.out.println("Установите версию приложения для Android по ссылке");
-         } else if (clientOS == 1 && clientDeviceYear < 2015) {
+         } else if (clientOS == 1 && clientDeviceYear < currentYear) {
              System.out.println("Установите облегченную версию приложения для Android по ссылке");
          }
          if (clientOS < 0 || clientOS > 2) {
